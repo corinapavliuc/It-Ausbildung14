c4d6beb6e67a2c6d66216efc2780ae92904b97bd
corinapavliuc <138014725+corinapavliuc@users.noreply.github.com>
2023-12-28 14:36:09 +0100

    Add files via upload

---
+++ test/java/com/be110723/Be110723ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be110723;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be110723ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
